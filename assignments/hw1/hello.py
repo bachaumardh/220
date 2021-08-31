09ec9a0826a5559bf58bc6c81f09fbc028cbed35
bachaumardh <bachaumardh@g.cofc.edu>
2021-08-31 17:54:51 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: <Hugo Bachaumard>
+hello.py
+
+Problem: Write a program that prints "hello,world!" to the screen.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
